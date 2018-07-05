d3a536abf7a7d7cadc9321041fb4d3d074830f18
Robin Trietsch <rtrietsch@bol.com>
2018-07-05 14:37:53 +0200

    Remove comment

--- list_bluetooth_devices.py
+++ list_bluetooth_devices.py
@@ -3,9 +3,6 @@ import sys
 from subprocess import check_output, Popen, PIPE
 import re
 
-# bluetooth_devices_raw = check_output(["/usr/libexec/PlistBuddy", "-c", "print :0:_items:0:device_title", "/dev/stdin <<< $(system_profiler SPBluetoothDataType -xml)"])
-
-
 bluetooth_devices_pipe = Popen(["/bin/bash", "-c", "/usr/libexec/PlistBuddy -c \"print :0:_items:0:device_title\" /dev/stdin <<< $(system_profiler SPBluetoothDataType -xml)"], stdout=PIPE)
 bluetooth_devices_raw, err = bluetooth_devices_pipe.communicate()
 
